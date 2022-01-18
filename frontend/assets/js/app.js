debad6c192d42fffb813218dc57d2a1bd7a8b71e
Andrei <barmalejj@googlemail.com>
2022-01-18 12:10:57 +0200

    Task 2

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,1 +1,4 @@
-import '../css/app.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/regular.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
+import '../scss/app.scss'
